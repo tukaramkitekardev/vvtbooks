4ae15b8cf3fcbee812738069599b9fbde33c2e57
Tukaram Kitekar <tukaramkitekardev@gmail.com>
2025-03-31 16:13:14 +0530

    hiagain

--- admin_header.php
+++ admin_header.php
@@ -15,14 +15,14 @@ if(isset($message)){
 
    <div class="flex">
 
-      <a href="admin_page.php" class="logo">Admin<span>Panel</span></a>
+      <a href="admin_page.php" class="logo">VVT Books | <span>AdminPanel</span></a>
 
       <nav class="navbar">
-         <a href="admin_page.php">home</a>
-         <a href="admin_products.php">products</a>
-         <a href="admin_orders.php">orders</a>
-         <a href="admin_users.php">users</a>
-         <a href="admin_contacts.php">messages</a>
+         <a href="admin_page.php">Home</a>
+         <a href="admin_products.php">Products</a>
+         <a href="admin_orders.php">Orders</a>
+         <a href="admin_users.php">Users</a>
+         <a href="admin_contacts.php">Messages</a>
       </nav>
 
       <div class="icons">
@@ -34,7 +34,7 @@ if(isset($message)){
          <p>username : <span><?php echo $_SESSION['admin_name']; ?></span></p>
          <p>email : <span><?php echo $_SESSION['admin_email']; ?></span></p>
          <a href="logout.php" class="delete-btn">logout</a>
-         <div>new <a href="login.php">login</a> | <a href="register.php">register</a></div>
+       
       </div>
 
    </div>
